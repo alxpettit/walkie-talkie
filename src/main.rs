56b06a8221ff16a4ab27ecf20c97d0896d0b41ab
Alexandria <alxpettit@gmail.com>
2023-01-14 00:30:46 -0700

    saddd

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
+use async_stream::stream;
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use cpal::{Device, StreamConfig};
 use futures::executor::block_on;
+use futures::{pin_mut, StreamExt};
+use futures_core::Stream;
 use nnnoiseless::DenoiseState;
 use std::error::Error;
+use std::future::Future;
 use std::hash::Hasher;
 use std::ops::Deref;
+use std::pin::Pin;
 use std::sync::{mpsc, Arc};
 use std::thread;
 use std::time::Duration;
@@ -22,23 +28,67 @@ impl MyDefault for [f32; DenoiseState::FRAME_SIZE] {
         [0.; DenoiseState::FRAME_SIZE]
     }
 }
-fn denoise_stream(mut rx: Receiver<f32>) -> Receiver<f32> {
+
+pub fn getstream_mic_input(
+    config: cpal::StreamConfig,
+    input_device: cpal::Device,
+) -> impl Stream<Item = f32> {
+    stream! {
+        let (tx, rx) = mpsc::channel::<Chunk>();
+
+        let input_stream = cpal::Device::build_input_stream(
+            &input_device, &config,  move |data: &[f32], _: &cpal::InputCallbackInfo| {
+            tx.send(data.to_vec()).unwrap();
+        }, move |_err| {}).expect("Failed to make stream :(");
+
+        input_stream.play().expect("Failed to play stream");
+
+        for data in rx {
+            for sample in data {yield sample;}
+        }
+    }
+}
+
+fn getstream_denoise<S: Stream<Item = f32> + Unpin>(mut input: S) -> impl Stream<Item = f32> {
     let denoise = RwLock::new(DenoiseState::new());
-    let (out_tx, mut out_rx) = tokio::sync::broadcast::channel::<f32>(100000);
-    let handle = thread::spawn(move || loop {
+    stream! {
         let mut frame_output: [f32; DenoiseState::FRAME_SIZE] = MyDefault::default();
         let mut frame_input: [f32; DenoiseState::FRAME_SIZE] = MyDefault::default();
         for s in &mut frame_input {
-            *s = block_on(rx.recv()).unwrap() * 32768.0;
+            *s = input.next().await.unwrap() * 32768.0;
         }
 
         block_on(denoise.write()).process_frame(&mut frame_output, &mut frame_input);
         for s in &frame_output {
-            out_tx.send(*s / 32768.0).unwrap();
+            yield *s / 32768.0;
         }
-    });
+    }
+}
 
-    out_rx
+fn stream_to_speaker<S: Stream<Item = f32> + Unpin>(
+    config: StreamConfig,
+    output_device: Device,
+    mut input: S,
+) {
+    let (tx, rx) = mpsc::channel::<f32>();
+    let out_stream = output_device
+        .build_output_stream(
+            &config,
+            move |output: &mut [f32], _| {
+                for output_sample in output {
+                    // This had better be zero cost >.>
+                    *output_sample = rx.recv().unwrap();
+                }
+            },
+            |_| {},
+        )
+        .expect("TODO: panic message");
+
+    out_stream.play().unwrap();
+
+    loop {
+        tx.send(block_on(input.next()).unwrap()).unwrap();
+    }
 }
 
 #[tokio::main]
@@ -55,45 +105,17 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let mut config: cpal::StreamConfig = supported_config.into();
     config.sample_rate = cpal::SampleRate(44_100);
 
-    let (tx, mut rx) = broadcast::channel::<f32>(480000);
-    let rx_denoise = tx.subscribe();
-
-    let input_stream = cpal::Device::build_input_stream(
-        &input_device,
-        &config,
-        move |data: &[f32], _: &cpal::InputCallbackInfo| {
-            for sample in data.to_vec() {
-                tx.send(sample).expect("TODO: panic message");
-            }
-        },
-        move |_err| {},
-    )?;
-    input_stream.play().expect("TODO: panic message");
-
-    let mut denoise_rx = denoise_stream(rx_denoise);
+    let mut mic_stream = getstream_mic_input(config.clone(), input_device);
+    pin_mut!(mic_stream);
+    let mut denoised_mic_stream = getstream_denoise(mic_stream);
+    pin_mut!(denoised_mic_stream);
 
     let output_device = host
         .default_output_device()
         .ok_or("No default output device available!")?;
-    let out_stream = output_device
-        .build_output_stream(
-            &config,
-            move |output: &mut [f32], _| {
-                for output_sample in output {
-                    // This had better be zero cost >.>
-                    match futures::executor::block_on(denoise_rx.recv()) {
-                        Ok(sample) => {
-                            *output_sample = sample;
-                        }
-                        Err(_) => {}
-                    }
-                }
-            },
-            |_| {},
-        )
-        .expect("TODO: panic message");
 
-    out_stream.play().unwrap();
+    stream_to_speaker(config, output_device, denoised_mic_stream);
+
     loop {
         thread::sleep(Duration::from_secs(10000));
     }
