8a8b7ea06e42cca6dd30cc8a35a5164c54c8c893
Alexandria <alxpettit@gmail.com>
2023-01-15 03:00:47 -0700

    :c no workie

--- src/mic/mod.rs
+++ src/mic/mod.rs
@@ -1,29 +1,33 @@
 use crate::*;
 use async_fn_stream::try_fn_stream;
+use futures::executor::block_on;
 
 pub fn getstream_from_mic(
     config: cpal::StreamConfig,
     input_device: cpal::Device,
 ) -> impl Stream<Item = PCMResult> {
     try_fn_stream(|emitter| async move {
-        let (tx, rx) = mpsc::channel::<PCMVec>();
+        // let (tx, rx) = mpsc::channel::<PCMVec>();
 
         let input_stream = cpal::Device::build_input_stream(
             &input_device,
             &config,
             move |data: &[f32], _: &cpal::InputCallbackInfo| {
-                tx.send(data.to_vec()).unwrap();
+                for sample in data {
+                    block_on(emitter.emit(*sample));
+                }
+                //   tx.send(data.to_vec()).unwrap();
             },
             move |_err| {},
         )?;
 
         input_stream.play()?;
 
-        for data in rx {
-            for sample in data {
-                emitter.emit(sample).await;
-            }
-        }
+        // for data in rx {
+        //     for sample in data {
+        //         emitter.emit(sample).await;
+        //     }
+        // }
         Ok(())
     })
 }
