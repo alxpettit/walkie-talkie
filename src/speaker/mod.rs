d19ac490e22d58ef802b45edeb95c46e4c40ba6f
Alexandria <alxpettit@gmail.com>
2023-01-21 15:19:40 -0700

    sleepy

--- src/speaker/mod.rs
+++ src/speaker/mod.rs
@@ -1,14 +1,37 @@
 use crate::*;
 
-use async_fn_stream::try_fn_stream;
+use async_fn_stream::{fn_stream, try_fn_stream};
+use snafu::prelude::*;
+use std::sync::mpsc::SendError;
+use cpal::StreamError;
 
-pub fn getstream_to_speaker<S: Stream<Item = PCMResult> + Unpin>(
+#[derive(Debug, Snafu)]
+pub enum SpeakerError {
+    #[snafu(display("StreamError: {}", e))]
+    StreamError { e: cpal::StreamError },
+    #[snafu(display("Error sending stream: {}", e))]
+    StreamSendingError { e: SendError<f32> },
+}
+
+impl From<StreamError> for SpeakerError {
+    fn from(e: StreamError) -> Self {
+        SpeakerError::StreamError { e }
+    }
+}
+
+
+pub fn getstream_to_speaker<S, E>(
     config: StreamConfig,
     output_device: Device,
+    mut error_callback: E,
     mut input: S,
-) -> impl Stream<Item = PCMResult> {
+) -> impl Stream<Item = PCMUnit>
+where
+    E: FnMut(SpeakerError) + Send + 'static,
+    S: Stream<Item = PCMUnit> + Unpin,
+{
     let (tx, rx) = mpsc::channel::<f32>();
-    try_fn_stream(|emitter| async move {
+    fn_stream(|emitter| async move {
         let out_stream = output_device.build_output_stream(
             &config,
             move |output: &mut [f32], _| {
@@ -16,16 +39,15 @@ pub fn getstream_to_speaker<S: Stream<Item = PCMResult> + Unpin>(
                     *output_sample = rx.recv().unwrap();
                 }
             },
-            |_| {},
+            move |e| error_callback(e.into()),
         )?;
 
         out_stream.play()?;
 
         while let Some(next_input) = input.next().await {
-            let inp: f32 = next_input?;
-            tx.send(inp)?;
+            let inp: f32 = next_input;
+            tx.send(inp).or_else(|x| );
             emitter.emit(inp).await;
         }
-        Ok(())
     })
 }
