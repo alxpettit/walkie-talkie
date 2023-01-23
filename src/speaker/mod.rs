bbab11a7d26ad1100341d11746a9aec0578265c4
Alexandria <alxpettit@gmail.com>
2023-01-22 21:09:13 -0700

    dumb

--- src/speaker/mod.rs
+++ src/speaker/mod.rs
@@ -1,7 +1,7 @@
 use crate::*;
 
 use async_fn_stream::{fn_stream, try_fn_stream};
-use cpal::StreamError;
+use cpal::{BuildStreamError, PlayStreamError, StreamError};
 use snafu::prelude::*;
 use std::sync::mpsc::{Receiver, SendError};
 
@@ -11,6 +11,10 @@ pub enum SpeakerError {
     StreamError { e: cpal::StreamError },
     #[snafu(display("Error sending stream: {}", e))]
     StreamSendingError { e: SendError<f32> },
+    #[snafu(display("Build stream error: {}", e))]
+    BuildStreamError { e: BuildStreamError },
+    #[snafu(display("Play stream error: {}", e))]
+    PlayStreamError { e: PlayStreamError },
 }
 
 impl From<StreamError> for SpeakerError {
@@ -18,39 +22,71 @@ impl From<StreamError> for SpeakerError {
         SpeakerError::StreamError { e }
     }
 }
+// the trait `From<std::sync::mpsc::SendError<f32>>` is not implemented for `SpeakerError`
+
+impl From<SendError<f32>> for SpeakerError {
+    fn from(e: SendError<f32>) -> Self {
+        SpeakerError::StreamSendingError { e }
+    }
+}
+
+impl From<BuildStreamError> for SpeakerError {
+    fn from(e: BuildStreamError) -> Self {
+        SpeakerError::BuildStreamError { e }
+    }
+}
+
+impl From<PlayStreamError> for SpeakerError {
+    fn from(e: PlayStreamError) -> Self {
+        SpeakerError::PlayStreamError { e }
+    }
+}
+
+#[cfg(feature = "error_stream")]
+type ResultStream = (Box<dyn Stream<Item = PCMUnit>>, Receiver<SpeakerError>);
+
+#[cfg(not(feature = "error_stream"))]
+type ResultStream = Box<dyn Stream<Item = PCMUnit>>;
 
 pub fn getstream_to_speaker<S>(
     config: StreamConfig,
     output_device: Device,
     mut input: S,
-) -> (impl Stream<Item = PCMUnit>, Receiver<SpeakerError>)
+) -> ResultStream
 where
     S: Stream<Item = PCMResult> + Unpin,
 {
+    #[cfg(feature = "error_stream")]
     let (tx_err, rx_err) = mpsc::channel::<SpeakerError>();
     let (tx, rx) = mpsc::channel::<f32>();
-    (
-        fn_stream(|emitter| async move {
-            let out_stream = output_device
-                .build_output_stream(
-                    &config,
-                    move |output: &mut [f32], _| {
-                        for output_sample in output {
-                            *output_sample = rx.recv().unwrap();
-                        }
-                    },
-                    |e| tx_err.send(e.into()),
-                )
-                .unwrap();
+    let stream = Box::new(fn_stream(|emitter| async move {
+        #[cfg(feature = "error_stream")]
+        let tx_err_ptr = tx_err.clone();
+        let out_stream = output_device.build_output_stream(
+            &config,
+            move |output: &mut [f32], _| {
+                for output_sample in output {
+                    *output_sample = rx.recv().unwrap();
+                }
+            },
+            move |e| {
+                #[cfg(feature = "error_stream")]
+                tx_err_ptr.send(e.into()).unwrap();
+            },
+        );
 
-            out_stream.play().unwrap();
+        out_stream.unwrap().play().unwrap();
 
-            while let Some(next_input) = input.next().await {
-                let inp: f32 = next_input.unwrap();
-                tx.send(inp).unwrap();
-                emitter.emit(inp).await;
-            }
-        }),
-        rx_err,
-    )
+        while let Some(next_input) = input.next().await {
+            let inp: f32 = next_input.unwrap();
+            tx.send(inp).unwrap();
+            emitter.emit(inp).await;
+        }
+    }));
+
+    #[cfg(feature = "error_stream")]
+    return (stream, rx_err);
+
+    #[cfg(not(feature = "error_stream"))]
+    stream
 }
