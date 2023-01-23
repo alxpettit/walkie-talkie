a7095168ff86814d9a257853d845f06dcfa340f3
Alexandria <alxpettit@gmail.com>
2023-01-22 23:25:00 -0700

    we need unit testing!

--- src/mic/mod.rs
+++ src/mic/mod.rs
@@ -27,3 +27,63 @@ pub fn getstream_from_mic(
         Ok(())
     })
 }
+
+fn convert_to_u8(buffer: &[f32]) -> Vec<u8> {
+    buffer
+        .iter()
+        .flat_map(|&x| {
+            let sample = (x * std::i16::MAX as f32) as i16;
+            vec![(sample >> 8) as u8, (sample & 0xff) as u8]
+        })
+        .collect()
+}
+
+mod tests {
+    use super::*;
+    use futures::executor::block_on;
+    use hound::WavSpec;
+    use hound::WavWriter;
+    use std::io::Write;
+    use std::time::{Duration, Instant};
+
+    #[test]
+    fn it_works() -> Result<(), Box<dyn Error>> {
+        let host = cpal::default_host();
+        let input_device = host
+            .default_input_device()
+            .ok_or("No default input device available :c")?;
+        let mut supported_configs_range = input_device.supported_input_configs()?;
+        let supported_config = supported_configs_range
+            .next()
+            .ok_or("Could not get the first supported config from range")?
+            .with_max_sample_rate();
+        let mut config: cpal::StreamConfig = supported_config.into();
+        config.sample_rate = cpal::SampleRate(44_100);
+        config.channels = 2u16;
+
+        let mut tmp = std::env::temp_dir();
+        tmp.push("getstream_from_mic_test.wav");
+
+        let mut wav_writer = WavWriter::create(
+            tmp,
+            WavSpec {
+                channels: config.channels,
+                sample_rate: config.sample_rate.0, // Dynamically grab
+                bits_per_sample: 32,               // Hound locks this at 32
+                sample_format: hound::SampleFormat::Float,
+            },
+        )?;
+
+        let start = Instant::now();
+        let stream = getstream_from_mic(config, input_device);
+        pin_mut!(stream);
+        while let Some(chunk) = block_on(stream.next()) {
+            if start.elapsed() > Duration::from_secs(5) {
+                break;
+            }
+            wav_writer.write_sample(chunk.unwrap()).unwrap();
+        }
+
+        Ok(())
+    }
+}
