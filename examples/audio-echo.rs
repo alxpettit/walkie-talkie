3d5aa8b3cef27a584fdf2dbae8a8625c21496da7
Alexandria <alxpettit@gmail.com>
2023-02-08 10:53:47 -0700

    ok

--- examples/audio-echo.rs
+++ examples/audio-echo.rs
@@ -26,32 +26,14 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let mut config: cpal::StreamConfig = supported_config.into();
     config.sample_rate = cpal::SampleRate(88_200); // cpal::SampleRate(44_100);
 
-    mk_gen!(let mic_stream = getstream_from_mic(config.clone(), input_device));
-
-    mk_gen!(let denoised_mic_stream = getstream_denoise(mic_stream));
-
     let output_device = host
         .default_output_device()
         .ok_or("No default output device available!")?;
 
-    // let mic_stream = getstream_fft(mic_stream);
-    // pin_mut!(mic_stream);
-    // //
-    // println!("henlo");
-    // while let Some(buf) = complex.next().await {
-    //     println!("{:#?}", buf);
-    // }
-
-    mk_gen!(let fft_stream = getstream_fft(denoised_mic_stream));
-
-    // mk_gen!(let stream_to_speaker =
+    mk_gen!(let mic_stream = getstream_from_mic(config.clone(), input_device));
+    mk_gen!(let mic_stream = getstream_denoise(mic_stream));
+    mk_gen!(let fft_stream = getstream_fft(mic_stream));
     getstream_to_speaker(config, output_device, fft_stream);
-    // );
-    // for s in stream_to_speaker {
-    //     // if let Err(e) = i {
-    //     //     println!("{}", e);
-    //     // }
-    //     //println!("{}", i.unwrap());
-    // }
+
     Ok(())
 }
